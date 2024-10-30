083f0ff042b7ed0df92bc0ef58616eeec92f3b09
AntonRaevsky <aanraevskiy@sberbank.ru>
2024-10-30 17:05:44 +0300

    test height img

--- src/scripts/templates/extendedCardList.js
+++ src/scripts/templates/extendedCardList.js
@@ -1,73 +1,75 @@
 function getExtendedCardList() {
 	return {
 		"card": {
-		"type": "extended_list_card",
-			"paddings": {
-			"left": "9x",
-				"top": "9x",
-				"right": "9x",
-				"bottom": "9x"
-		},
-		"cell_image": {
-			"url": "https://content.sberdevices.ru/smartmarket-smide-prod/1334030/1248280/RQE47TcgVJJ3IW68.png",
-				"hash": "84fed454b566e2e89b304d74a62e4456",
-				"placeholder_color": "solid_black",
-				"actions": [
-				{ }
-			],
-			"fixed_ratio": "3x2"
-		},
-		"cells": [
-			{
-				"type": "text_cell_view",
-				"content": {
-					"text": "Киану Ривс",
-					"typeface": "headline1",
-					"text_color": "default",
-					"max_lines": 0
-				},
+			"type": "extended_list_card",
 				"paddings": {
-					"left": "8x",
-					"top": "10x",
-					"right": "8x"
-				}
+				"left": "9x",
+					"top": "9x",
+					"right": "9x",
+					"bottom": "9x"
 			},
-			{
-				"type": "text_cell_view",
-				"content": {
-					"text": "Канадский актёр, кинопродюсер, и музыкант. Наиболее известен своими ролями в киносериях «Матрица» и «Джон Уик»...",
-					"typeface": "headline3",
-					"text_color": "default",
-					"max_lines": 0
-				},
-				"paddings": {
-					"left": "8x",
-					"top": "10x",
-					"right": "8x"
-				}
-			},
-			{
-				"type": "text_cell_view",
-				"content": {
-					"text": "ru.wikipedia.org",
-					"typeface": "headline3",
-					"text_color": "default",
-					"max_lines": 0,
+			"cell_image": {
+				"url": "https://content.sberdevices.ru/smartmarket-smide-prod/1334030/1248280/RQE47TcgVJJ3IW68.png",
+					"hash": "84fed454b566e2e89b304d74a62e4456",
+					"placeholder_color": "solid_black",
 					"actions": [
-						{
-							"type": "deep_link",
-							"deep_link": "https://ru.wikipedia.org"
+					{ }
+				],
+				"fixed_ratio": "1/1",
+				"height": "180px",
+				"width": "180px",
+			},
+			"cells": [
+				{
+					"type": "text_cell_view",
+					"content": {
+						"text": "Киану Ривс",
+						"typeface": "headline1",
+						"text_color": "default",
+						"max_lines": 0
+					},
+					"paddings": {
+						"left": "8x",
+						"top": "10x",
+						"right": "8x"
+					}
+				},
+				{
+					"type": "text_cell_view",
+					"content": {
+						"text": "Канадский актёр, кинопродюсер, и музыкант. Наиболее известен своими ролями в киносериях «Матрица» и «Джон Уик»...",
+						"typeface": "headline3",
+						"text_color": "default",
+						"max_lines": 0
+					},
+					"paddings": {
+						"left": "8x",
+						"top": "10x",
+						"right": "8x"
+					}
+				},
+				{
+					"type": "text_cell_view",
+					"content": {
+						"text": "ru.wikipedia.org",
+						"typeface": "headline3",
+						"text_color": "default",
+						"max_lines": 0,
+						"actions": [
+							{
+								"type": "deep_link",
+								"deep_link": "https://ru.wikipedia.org"
+							}
+						],
+						"margins": {
+							"left": "10x",
+							"top": "5x",
+							"right": "10x",
+							"bottom": "5x"
 						}
-					],
-					"margins": {
-						"left": "10x",
-						"top": "5x",
-						"right": "10x",
-						"bottom": "5x"
 					}
 				}
-			}
-		]
-	}
+			]
+		}
 	}
 }
